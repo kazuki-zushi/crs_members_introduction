264ce90658050c9f137acfd79136d2cb367b4861
圖子和希 <zushikazuki@zushikazukinoMacBook-Air.local>
2024-05-11 18:52:08 +0900

    vue add vuetifyする前

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="app">
+    <VueCrs />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueCrs from './components/VueCrs.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueCrs,
   }
 }
 </script>
