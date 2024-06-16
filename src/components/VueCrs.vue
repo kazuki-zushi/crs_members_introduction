8b2bd1ed4e7567725981945ca7cb6febfd597115
圖子和希 <zushikazuki@zushikazukinoMacBook-Air.local>
2024-06-17 01:06:12 +0900

    VueCrs.vue を更新し、画像を追加

--- src/components/VueCrs.vue
+++ src/components/VueCrs.vue
@@ -1,38 +1,81 @@
 <template>
   <v-app id="inspire">
     <v-app-bar extended>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-app-bar-title>Application</v-app-bar-title>
-
+            <!-- 反映されていない -->
+      <v-img src="/vue-crs/img/logo_levtech.png" height="40" alt="Levtech Logo">Levtech</v-img>
       <v-spacer></v-spacer>
 
       <v-btn icon="mdi-dots-vertical">
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main class="background">
       <v-container>
         <v-row>
           <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
+            v-for="person in team"
+            :key="person.avatar"
+            cols="3"
+            class="profile-col"
           >
-            <v-card height="200"></v-card>
+            <v-card class="profile-card">
+              <v-responsive ratio="4/3">
+                <v-img :src="person.avatar" alt="Profile Image" class="profile-img"></v-img>
+              </v-responsive>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
+    <v-footer>
+    </v-footer>
   </v-app>
 </template>
+
 
 <script>
   export default {
     data() {
-      return {
-        name: ""
+      return { 
+        team: [
+        { avatar:"/vue-crs/img/avatar-1.png"},
+        { avatar:"/vue-crs/img/avatar-2.png"},
+        { avatar:"/vue-crs/img/avatar-3.png"},
+        { avatar:"/vue-crs/img/avatar-4.png"}
+        ]
+       
       }
     }
   }
 </script>
+
+
+<style>
+.profile-card {
+  width: 259px;
+  height: 337px;
+  /* 反映されていない */
+  border-radius: 30px;
+  background-color: rgba(235, 123, 167, 0.5);
+  overflow: hidden;
+  /* Ensure the image fills the card */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.profile-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;  
+}
+
+.profile-col {
+  margin-bottom: 40px;
+}
+
+.background{
+background-color: #FFFAEB;
+}
+
+</style>
